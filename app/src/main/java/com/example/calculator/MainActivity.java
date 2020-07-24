1689d327bc56c90a042bf393d38664e1df4667e0
Krishnaprasad24 <anandhakrishna2624@gmail.com>
2020-07-24 13:10:29 +0530

    Krishna (#1)
    * Add app
    
    * Add app
    
    * calc app
    
    * calc app

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.calculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Attempt to launch addition
+        Button cadd = (Button) findViewById(R.id.cadd);
+        cadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(),MainActivity2.class);
+                startIntent.putExtra("com.example.calculator.SOMETHING","HELLO WORLD");
+                startActivity(startIntent);
+            }
+        });
+
+        //Attempt to launch multiplication
+        Button cmulti = (Button) findViewById(R.id.cmulti);
+        cmulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(),MainActivity3.class);
+                startIntent.putExtra("com.example.calculator.SOMETHING","HELLO WORLD");
+                        startActivity(startIntent);
+            }
+        });
+
+        //Attempt to launch random number
+        Button crand = (Button) findViewById(R.id.crand);
+        crand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(),MainActivity4.class);
+                startIntent.putExtra("com.example.calculator.SOMETHING","HELLO WORLD");
+                        startActivity(startIntent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/calculator/MainActivity4.java
@@ -0,0 +1,42 @@
+package com.example.calculator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+        if (getIntent().hasExtra("com.example.calculator.SOMETHING")) {
+
+
+            Button ranBtn = (Button) findViewById(R.id.rndButton);
+            ranBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    EditText firstnumEdittext = (EditText) findViewById(R.id.fisrtnumText);
+                    EditText secondnumEdittext = (EditText) findViewById(R.id.secondnumText);
+                    TextView resTextview = (TextView) findViewById(R.id.restextView);
+                    int startvalue = Integer.parseInt(firstnumEdittext.getText().toString());
+                    int endvalue = Integer.parseInt(secondnumEdittext.getText().toString());
+                    if (startvalue > endvalue) {
+                        int temp = startvalue;
+                        startvalue = endvalue;
+                        endvalue = temp;
+                    }
+
+                    int result = (int) (Math.random() * (endvalue - startvalue + 1) + startvalue);
+                    String res = Integer.toString(result);
+                    resTextview.setText(res);
+                }
+            });
+        }
+    }
+}
