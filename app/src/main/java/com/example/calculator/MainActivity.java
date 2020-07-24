655e87fd673cb2b01d6086421ea811a81f4ead02
Krishnaprasad24 <67697318+Krishnaprasad24@users.noreply.github.com>
2020-07-24 12:33:56 +0530

    calc app

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.calculator;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,18 +13,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button addBtn = (Button) findViewById(R.id.addBtn);
-        addBtn.setOnClickListener(new View.OnClickListener() {
+        //Attempt to launch addition
+        Button cadd = (Button) findViewById(R.id.cadd);
+        cadd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText firstNumEditText = (EditText) findViewById(R.id.firstNumEditText);
-                EditText secondNumEditText = (EditText) findViewById(R.id.secondNumEditText);
-                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+                Intent startIntent = new Intent(getApplicationContext(),MainActivity2.class);
+                startIntent.putExtra("com.example.calculator.SOMETHING","HELLO WORLD");
+                startActivity(startIntent);
+            }
+        });
 
-                int num1 = Integer.parseInt(firstNumEditText.getText().toString());
-                int num2 = Integer.parseInt(secondNumEditText.getText().toString());
-                int result = num1 + num2;
-                resultTextView.setText(result + "");
+        //Attempt to launch multiplication
+        Button cmulti = (Button) findViewById(R.id.cmulti);
+        cmulti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(),MainActivity3.class);
+                startIntent.putExtra("com.example.calculator.SOMETHING","HELLO WORLD");
+                        startActivity(startIntent);
+            }
+        });
+
+        //Attempt to launch random number
+        Button crand = (Button) findViewById(R.id.crand);
+        crand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(),MainActivity4.class);
+                startIntent.putExtra("com.example.calculator.SOMETHING","HELLO WORLD");
+                        startActivity(startIntent);
             }
         });
     }
